b48e920c6f55516d042725903cb930ee2d953177
Isaac Agaba <isaac.agaba@pipedrive.com>
2019-11-21 12:23:35 +0200

    Add Content section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from './components/Map/';
+import Content from './components/Content/';
 
 function App() {
   return (
-      <div>
-        <Map />
+      <div className="parent">
+       <Content/>
+        <Map/>
       </div>
   );
 }
---
+++ src/components/Content/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+const Content = () => {
+
+    return (
+            <div className="container"><h1>Here goes the content</h1></div>
+    );
+};
+
+export default Content;
