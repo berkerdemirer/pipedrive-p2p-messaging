315c48cf82599edb6c2f2b55c838814fcf997588
Isaac Agaba <isaac.agaba@pipedrive.com>
2019-11-21 22:19:48 +0200

    Remove console :)

--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -8,7 +8,6 @@ function Topics({topics, topicChange}) {
     return <div className="filter-container">
         {topics.map((topic,index) => (
             <div className="filter-container-item" key={index}>
-                {console.log('selected',selected)}
             <div className="filter-container-item-icon">
                 <Avatar src={topic.img} round="50px" size="30" fgColor="red" color="#d1dff0" />
             </div>
