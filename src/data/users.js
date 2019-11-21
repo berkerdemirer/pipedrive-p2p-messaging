111276e3a9b3ae82e4cda258db33a0f3909e5b51
Oleh Matsuk <oleh.matsuk@pipedrive.com>
2019-11-21 13:04:31 +0200

    Move data

--- src/data/users.js
+++ src/data/users.js
@@ -2,26 +2,31 @@ module.exports = {
 	1: {
 		name: 'Lex Brown',
 		company: 'Microsoft',
+		avatar: '/assets/avatars/user1.jpeg',
 		location: { lat: 27.825941, lng: -82.711333 },
 	},
 	2: {
 		name: 'Adam Bermudez',
 		company: 'Taxify',
+		avatar: '/assets/avatars/user2.jpeg',
 		location: { lat: 27.88713, lng: -82.797145 },
 	},
 	3: {
 		name: 'Coriena Merejo',
 		company: 'Uber',
+		avatar: '/assets/avatars/user3.jpeg',
 		location: { lat: 27.821588, lng: -82.63569 },
 	},
 	4: {
 		name: 'Jodie Rose',
 		company: 'Bank of America',
+		avatar: '/assets/avatars/user4.jpeg',
 		location: { lat: 27.770744, lng: -82.729506 },
 	},
 	5: {
 		name: 'Samantha Dedea',
 		company: 'Blizzard Entertainment',
+		avatar: '/assets/avatars/user5.jpeg',
 		location: { lat: 27.984245, lng: -82.762648 },
 	},
 };
