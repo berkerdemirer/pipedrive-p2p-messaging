cebfea534195713f6084f5024daf38c1a73fae20
Oleh Matsuk <oleh.matsuk@pipedrive.com>
2019-11-21 18:41:29 +0200

    Fix timestamps

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,25 +1,31 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import useInterval from '@use-it/interval';
 import PropTypes from 'prop-types';
 import './index.css';
 import Avatar from 'react-avatar';
-import users from "../../data/users"
-const Message = ({data}) => {
-    const user = users[data.userId];
-    useEffect(() => {
+import users from '../../data/users';
 
-    }, []);
-    return (
-        <div className="message-container">
-            <Avatar src={user.avatar} round="50px" size="50" fgColor="#357edd" name='T' color="#d1dff0" />
-            <div className="message-data">
-                <p className="message-data-user">{user.name}</p>
-                <span className="message-data-time">{data.sentAt.fromNow()}</span>
-                <p>{data.content}</p>
-            </div>
-        </div>
-    );
+const Message = ({ data }) => {
+    const user = users[data.userId];
+    const [sentAt, setSentAt] = useState(data.sentAt.fromNow());
+    useEffect(() => {}, []);
+
+	useInterval(() => {
+		setSentAt(data.sentAt.fromNow());
+	}, 1000);
+
+	return (
+		<div className="message-container">
+			<Avatar src={user.avatar} round="50px" size="50" fgColor="#357edd" name="T" color="#d1dff0" />
+			<div className="message-data">
+				<p className="message-data-user">{user.name}</p>
+				<span className="message-data-time">{sentAt}</span>
+				<p>{data.content}</p>
+			</div>
+		</div>
+	);
 };
 Message.propTypes = {
-    data: PropTypes.object.isRequired,
+	data: PropTypes.object.isRequired,
 };
 export default Message;
--- src/data/messages.js
+++ src/data/messages.js
@@ -1,56 +1,56 @@
 import moment from 'moment'
-const demoStart = moment();
+const start = moment();
 
 const messages = [
     {
         userId: 1,
         content: 'hey, anyone seen that Joker movie? is it any good?',
-        sentAt: demoStart.subtract(12, 'minutes'),
+        sentAt: moment(start).subtract(12, 'minutes'),
         thread: [
             {
                 userId: 2,
                 content: `it's... weird. but p good overall`,
-                sentAt: demoStart.subtract(10, 'minutes').subtract(43, 'seconds'),
+                sentAt: moment(start).subtract(10, 'minutes').subtract(43, 'seconds'),
             }
         ]
     },
     {
         userId: 2,
         content: 'I guess this is one of THOSE days... Anyone else could use a cheer-me-up right now?',
-        sentAt: demoStart.subtract(6, 'minutes').subtract(11, 'seconds'),
+        sentAt: moment(start).subtract(6, 'minutes').subtract(11, 'seconds'),
         thread: [
             {
                 userId: 1,
                 content: 'hang in there. you just need 1 win to get right back on the horse. you got it ;)',
-                sentAt: demoStart.subtract(1, 'minutes').subtract(44, 'seconds'),
+                sentAt: moment(start).subtract(1, 'minutes').subtract(44, 'seconds'),
             }
         ],
     },
     {
         userId: 3,
         content: `I can't find my deals... :(`,
-        sentAt: demoStart.subtract(5, 'minutes').subtract(35, 'seconds'),
+        sentAt: moment(start).subtract(5, 'minutes').subtract(35, 'seconds'),
         thread: [],
     }, {
         userId: 3,
         content: 'Nevermind, got it :)',
-        sentAt: demoStart.subtract(5, 'minutes').subtract(10, 'seconds'),
+        sentAt: moment(start).subtract(5, 'minutes').subtract(10, 'seconds'),
         thread: [],
     }, {
         userId: 4,
         content: `What's a good book on time managment you've read recently??`,
-        sentAt: demoStart.subtract(3, 'minutes').subtract(58, 'seconds'),
+        sentAt: moment(start).subtract(3, 'minutes').subtract(58, 'seconds'),
         thread: [
             {
                 userId: 2,
                 content: `'The ONE thing' - definitely`,
-                sentAt: demoStart.subtract(2, 'minutes').subtract(11, 'seconds'),
+                sentAt: moment(start).subtract(2, 'minutes').subtract(11, 'seconds'),
             },
             {
                 userId: 1,
                 content: `i would recommend "why we sleep". dont get confused by the title -
                     its what any person in todays hectic world should have in their home library`,
-                sentAt: demoStart.subtract(1, 'minutes').subtract(1, 'seconds'),
+                sentAt: moment(start).subtract(1, 'minutes').subtract(1, 'seconds'),
             }
         ]
     }
