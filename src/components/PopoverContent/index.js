1a79510ce1bb1bc53cfbaabfd550dc4fc4f4d8cb
Berker <berker.demirer@pipedrive.com>
2019-11-22 10:36:23 +0200

    popover content update

--- src/components/PopoverContent/index.js
+++ src/components/PopoverContent/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './index.css'
 
 const PopoverContent = ({user}) => {
     return (
       <div className="popover-container">
-          <div className="name">{user.name} is nearby</div>
-          <div>Interested in having a coffee</div>
-          <div>Interested talking about leads</div>
-          <button>Get in Touch</button>
+          <div className="popover-text"><b>{user.name} is nearby</div>
+          <div className="popover-text">Interested in having a coffee</div>
+          <div className="popover-text">Interested talking about leads</div>
+          <button className="get-in-touch">Get in Touch</button>
       </div>
     );
 };
