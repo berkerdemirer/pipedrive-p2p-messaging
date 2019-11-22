24a69879c0d41744755b0178ff063a23aedf7a64
Oleh Matsuk <oleh.matsuk@pipedrive.com>
2019-11-22 14:46:44 +0200

    Make scrolling fast

--- src/data/add-messages.js
+++ src/data/add-messages.js
@@ -1,21 +1,13 @@
 import delay from 'delay';
 import moment from 'moment';
-import { animateScroll } from "react-scroll";
 import { addTopMessage, addThreadMessage } from '../utils/messages';
 
 const seconds = s => s * 1000
 
-const addTopMessageAndScroll = (setMessages, message) => {
-    setMessages(messages => addTopMessage(messages, message));
-    animateScroll.scrollToBottom({
-        containerId: "messages-list"
-    });
-};
-
 const addMessages = async (setMessages) => {
     await delay(seconds(2));
 
-    addTopMessageAndScroll(setMessages, {
+    setMessages(messages => addTopMessage(messages, {
         id: 'b43e4921-392e-4bb2-b711-7ff357e82f12',
         userId: 5,
         content: `What's good my fellow sales peeps?`,
@@ -23,11 +15,11 @@ const addMessages = async (setMessages) => {
         thread: [],
         upvotes: [],
         topic: 3,
-    });
+    }));
 
     await delay(seconds(1));
 
-    addTopMessageAndScroll(setMessages, {
+    setMessages(messages => addTopMessage(messages, {
         id: 'd32fb16b-b268-4ea1-87c1-9a99463fc9ec',
         userId: 3,
         content: `Does anyone know how to close a deal? :)`,
@@ -35,11 +27,11 @@ const addMessages = async (setMessages) => {
         thread: [],
         upvotes: [],
         topic: 2,
-    });
+    }));
 
     await delay(seconds(4));
 
-    addTopMessageAndScroll(setMessages, {
+    setMessages(messages => addTopMessage(messages, {
         id: '0904cef1-db68-4157-8853-3d27d4697d37',
         userId: 3,
         content: `Where did I leave my keys???`,
@@ -47,7 +39,7 @@ const addMessages = async (setMessages) => {
         thread: [],
         upvotes: [],
         topic: 2,
-    });
+    }));
 };
 
 
